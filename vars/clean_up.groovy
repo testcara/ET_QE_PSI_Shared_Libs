41d37e741ac047437eb0665399c9bf7598837d7a
Cara Wang <wlin@redhat.com>
2019-05-20 16:07:29 +0800

    Set the token as parameter

--- vars/clean_up.groovy
+++ vars/clean_up.groovy
@@ -1,21 +1,25 @@
-def call(){
-    echo "--- Delete apps --->"
-    def etAppNames = ["cucumber-et-1", "cucumber-et-2", "cucumber-et-3"]
-    def mysqlAppNames = ["cucumber-et-1-mysql", "cucumber-et-2-mysql", "cucumber-et-3-mysql"]
-    etAppNames.each { app ->
-        openshift.selector("all", [ app : "$app" ]).delete()
-    }
-    mysqlAppNames.each { app ->
-        openshift.selector("all", [ app : "$app" ]).delete()
-    }
-    def exist1 = openshift.selector("template", "$templateNameofET").exists()
-    if (exist1) {
-        echo "--- Delete ET template --->"
-        openshift.selector("template", "$templateNameofET").delete()
-    } //if
-    def exist2 = openshift.selector("template", "$templateNameofMysql").exists()
-    if (exist2) {
-        echo "--- Delete Mysql template --->"
-        openshift.selector("template", "$templateNameofMysql").delete()
-    }
+def call(String token){
+    openshift.withCluster('https://paas.psi.redhat.com', token) {
+        openshift.withProject('errata-qe-test'){
+            echo "--- Delete apps --->"
+            def etAppNames = ["cucumber-et-1", "cucumber-et-2", "cucumber-et-3"]
+            def mysqlAppNames = ["cucumber-et-1-mysql", "cucumber-et-2-mysql", "cucumber-et-3-mysql"]
+            etAppNames.each { app ->
+                openshift.selector("all", [ app : "$app" ]).delete()
+            }
+            mysqlAppNames.each { app ->
+                openshift.selector("all", [ app : "$app" ]).delete()
+            }
+            def exist1 = openshift.selector("template", "$templateNameofET").exists()
+            if (exist1) {
+                echo "--- Delete ET template --->"
+                openshift.selector("template", "$templateNameofET").delete()
+            } //if
+            def exist2 = openshift.selector("template", "$templateNameofMysql").exists()
+            if (exist2) {
+                echo "--- Delete Mysql template --->"
+                openshift.selector("template", "$templateNameofMysql").delete()
+            }
+            } //project
+    } //cluster
 }
