0d8e578618f13f3bfacb2b87d4f7dd3cd7daabb3
Cara Wang <wlin@redhat.com>
2019-05-20 17:34:05 +0800

    Add 'get_pod_name_for_dc' and 'import sql files to db' lib
    With the 2 libs, we can import sql files to one mysql pod db

---
+++ vars/get_pod_name_for_dc.groovy
@@ -0,0 +1,7 @@
+def call(String token, String dcName){
+    openshift.withCluster('https://paas.psi.redhat.com', token) {
+    	openshift.withProject('errata-qe-test'){
+    		return openshift.selector("dc", dcName).related('pods').names[0]
+    	}
+    }
+}
