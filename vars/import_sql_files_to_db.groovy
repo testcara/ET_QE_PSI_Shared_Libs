9baaffdb2079b20120268165f427be298c27c373
Cara Wang <wlin@redhat.com>
2019-05-20 21:39:49 +0800

    Fix some errors for the 2 libs

--- vars/import_sql_files_to_db.groovy
+++ vars/import_sql_files_to_db.groovy
@@ -1,7 +1,7 @@
-def call(String token, String sqlFile, String podName, String mysqlUser, String MysqlPassword){
+def call(String token, String podName, String sqlFile, String mysqlUser, String mysqlPassword){
   openshift.withCluster('https://paas.psi.redhat.com', token) {
     openshift.withProject('errata-qe-test'){
-      sh "oc exec ${podName} -i -- mysql -u${mysqlUser} -p${MysqlPassword} -Derrata < ${sqlFile}"
+      sh "oc exec ${podName} -i -- mysql -u${mysqlUser} -p${mysqlPassword} -Derrata < ${sqlFile}"
     }
   }
 }
