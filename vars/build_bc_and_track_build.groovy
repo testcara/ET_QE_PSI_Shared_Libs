e4f3a9caf99b961abbe7761bf5d75897d983d2eb
Cara Wang <wlin@redhat.com>
2019-05-20 17:38:38 +0800

    Fix some errors for the build app functions

--- vars/build_bc_and_track_build.groovy
+++ vars/build_bc_and_track_build.groovy
@@ -1,20 +1,24 @@
-def call(Integer time, String bcName){
-    def goodBuildStatus = (String[]) ['Running', 'Pending', 'Complete']
-    def bcSelector = openshift.selector("bc", bcName)
-    bcSelector.startBuild()
-    def builds= bcSelector.related('builds')
-    timeout(time) { 
-        // Checking watch output and running watch closure again in 250ms
-        builds.untilEach(1) {
-            def status = it.object().status.phase
-            if ( goodBuildStatus.contains(status) == false ) {
-                throw new Exception("Build failed")
-            } //if
-            if ( status == 'Complete' )
-            {
-                echo "---> Build Complete ..."
-                return true
-            } //if
-        } //each
-    } //timeout
+def call(String token, Integer time, String bcName){
+    openshift.withCluster('https://paas.psi.redhat.com', token) {
+        openshift.withProject('errata-qe-test'){
+            def goodBuildStatus = (String[]) ['Running', 'Pending', 'Complete']
+            def bcSelector = openshift.selector("bc", bcName)
+            bcSelector.startBuild()
+            def builds= bcSelector.related('builds')
+            timeout(time) {
+                // Checking watch output and running watch closure again in 250ms
+                builds.untilEach(1) {
+                    def status = it.object().status.phase
+                    if ( goodBuildStatus.contains(status) == false ) {
+                        throw new Exception("Build failed")
+                    } //if
+                    if ( status == 'Complete' )
+                    {
+                        echo "---> Build Complete ..."
+                        return true
+                    } //if
+                } //each
+            } //timeout
+        } //project
+    } //cluster
 } //call
