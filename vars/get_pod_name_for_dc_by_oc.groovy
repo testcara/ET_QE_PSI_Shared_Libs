6f93f16c5c6058d43e8dbe5589e602f135f832e7
Cara Wang <wlin@redhat.com>
2019-05-22 20:00:54 +0800

    fix error

--- vars/get_pod_name_for_dc_by_oc.groovy
+++ vars/get_pod_name_for_dc_by_oc.groovy
@@ -1,7 +1,8 @@
 def call(String token, String dcName){
     openshift.withCluster('https://paas.psi.redhat.com', token) {
       openshift.withProject('errata-qe-test'){
-      	return sh "oc get pods | grep ${dcName} | cut -d ' ' -f 1"
+      	def pod = sh "oc get pods | grep ${dcName} | cut -d ' ' -f 1"
+      	return pod
       }
     }
 }
