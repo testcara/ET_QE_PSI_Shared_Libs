11436eacc889a88c084ba8e618a8d177e741527e
Cara Wang <wlin@redhat.com>
2019-05-22 19:20:02 +0800

    add oc shared lib

--- vars/specify_cucumber_umb_broker.groovy
+++ vars/specify_cucumber_umb_broker.groovy
@@ -5,5 +5,7 @@ def call(String token, String etPod){
   [ umb_config_1, umb_config_2 ].each {
     def change_umb_cmd="sed -i \"s/ENV\\['ET_UMB_BROKER_URL_1'\\]/'amqp:\\/\\/cucumber-umb-qe:5672'/g\" ${it}"
     run_cmd_against_pod(token, etPod, change_umb_cmd)
+    ef change_umb_cmd="sed -i \"s/ENV\\['ET_UMB_BROKER_URL_2'\\]/'amqp:\\/\\/cucumber-umb-qe:5672'/g\" ${it}"
+    run_cmd_against_pod(token, etPod, change_umb_cmd)
   }
 }
