4308920df140a75bb159146fcb41e951502bfb67
Cara Wang <wlin@redhat.com>
2019-05-22 19:31:54 +0800

    fix error

--- vars/deploy_dc_and_track_deployment_by_oc.groovy
+++ vars/deploy_dc_and_track_deployment_by_oc.groovy
@@ -2,7 +2,7 @@ def call(String token, Integer time, String dcName ){
     openshift.withCluster('https://paas.psi.redhat.com', token) {
         openshift.withProject('errata-qe-test'){
             echo "--- Deploy dc: ${dcName} --->"
-            sh "oc rollout --latest ${dcName}"
+            sh "oc rollout latest ${dcName}"
             def goodBuildStatus = (String[]) ['Running', 'Pending', 'Complete']
 		    def dcSelector = openshift.selector("dc", dcName)
 		    timeout(time) { 
