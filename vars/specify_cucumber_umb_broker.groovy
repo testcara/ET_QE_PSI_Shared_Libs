1a175f304119954b2cc532b079f903471dc4ae1c
Cara Wang <wlin@redhat.com>
2019-05-22 20:50:57 +0800

    fix error

--- vars/specify_cucumber_umb_broker.groovy
+++ vars/specify_cucumber_umb_broker.groovy
@@ -3,9 +3,9 @@ def call(String token, String etPod){
   def umb_config_1 = '/opt/app-root/src/config/initializers/credentials/message_bus.rb'
   def umb_config_2 = '/opt/app-root/src/examples/ruby/message_bus/umb_configuration.rb'
   [ umb_config_1, umb_config_2 ].each {
-    def change_umb_cmd="sed -i \"s/ENV\\['ET_UMB_BROKER_URL_1'\\]/'amqp:\\/\\/cucumber-umb-qe:5672'/g\" ${it}"
-    run_cmd_against_pod(token, etPod, change_umb_cmd)
-    ef change_umb_cmd="sed -i \"s/ENV\\['ET_UMB_BROKER_URL_2'\\]/'amqp:\\/\\/cucumber-umb-qe:5672'/g\" ${it}"
-    run_cmd_against_pod(token, etPod, change_umb_cmd)
+    def change_umb_cmd_1="sed -i \"s/ENV\\['ET_UMB_BROKER_URL_1'\\]/'amqp:\\/\\/cucumber-umb-qe:5672'/g\" ${it}"
+    run_cmd_against_pod(token, etPod, change_umb_cmd_1)
+    def change_umb_cmd_2="sed -i \"s/ENV\\['ET_UMB_BROKER_URL_2'\\]/'amqp:\\/\\/cucumber-umb-qe:5672'/g\" ${it}"
+    run_cmd_against_pod(token, etPod, change_umb_cmd_2)
   }
 }
