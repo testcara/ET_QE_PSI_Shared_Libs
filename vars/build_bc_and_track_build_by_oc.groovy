b28a77eb8bf25997df830d9bb8b3a90ebd9c1a01
Cara Wang <wlin@redhat.com>
2019-05-23 00:01:17 +0800

    Change the scripted to the oc command.
    For I always meet the following error when the parallel is used:
    watch closure threw an exception: "java.lang.NullPointerException:
    Cannot invoke method getServerUrl() on null object;
    Cannot invoke method getServerUrl() on null object".

--- vars/build_bc_and_track_build_by_oc.groovy
+++ vars/build_bc_and_track_build_by_oc.groovy
@@ -2,10 +2,34 @@ def call(String token, Integer time, String bcName){
     openshift.withCluster('https://paas.psi.redhat.com', token) {
         openshift.withProject('errata-qe-test'){
             sh "echo $bcName > bcName"
-            sh "oc start-build $bcName" 
+            sh "oc start-build $bcName"
+            sh "echo $bcName > bcName" 
+            sh '''
+            bcName=$(cat bcName)
+            # let us wait 30 mins
+            for i in {1..60}
+            do
+                sleep(30) # 30 seconds
+                status=$(oc get build | grep ${bcName} | awk "{print $4}")
+                if [[ ${status} == "Complete" ]]
+                then
+                  echo "---> Build complete ..."
+                  exit 0
+                elif [[ ${status} =~ "Failed" ]]
+                then
+                  echo "---> Build has been failed ..."
+                  exit 1
+                else
+                  echo "---> Still running ..."
+                fi
+            done
+            '''
+
+            /*
             def goodBuildStatus = (String[]) ['Running', 'Pending', 'Complete']
             def bcSelector = openshift.selector("bc", bcName)
             def builds= bcSelector.related('builds')
+
             timeout(time) {
                 // Checking watch output and running watch closure again in 250ms
                 builds.untilEach(1) {
@@ -20,6 +44,7 @@ def call(String token, Integer time, String bcName){
                     } //if
                 } //each
             } //timeout
+            */
         } //project
     } //cluster
 } //call
--- vars/deploy_dc_and_track_deployment_by_oc.groovy
+++ vars/deploy_dc_and_track_deployment_by_oc.groovy
@@ -3,8 +3,31 @@ def call(String token, Integer time, String dcName ){
         openshift.withProject('errata-qe-test'){
             echo "--- Deploy dc: ${dcName} --->"
             sh "oc rollout latest ${dcName}"
+            sh "echo $dcName > dcName" 
+            sh '''
+            dcName=$(cat dcName)
+            # let us wait 5 mins
+            for i in {1..30}
+            do
+                sleep(10) # 10 seconds
+                status=$(oc get pods | grep ${dcName} | grep -v build | grep -v deploy | awk "{print $3}")
+                if [[ ${status} == "Running" ]]
+                then
+                  echo "---> Deployment complete ..."
+                  exit 0
+                elif [[ ${status} =~ "Failed" ]] || [[ ${status} =~ "Error" ]]
+                then
+                  echo "---> Deployment has been failed ..."
+                  exit 1
+                else
+                  echo "---> Still running ..."
+                fi
+            done
+            '''
+            /*
             def goodBuildStatus = (String[]) ['Running', 'Pending', 'Complete']
 		    def dcSelector = openshift.selector("dc", dcName)
+
 		    timeout(time) { 
 		        openshift.selector("dc", dcName).related('pods').untilEach(1) {
 		            if (it.object().status.phase == "Running" )
@@ -14,6 +37,7 @@ def call(String token, Integer time, String dcName ){
 		            }
 		        } //each
 		    } // timeout
+		    */
         } //project
     } //cluster
 } //call
