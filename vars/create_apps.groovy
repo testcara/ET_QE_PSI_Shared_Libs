a771eabf509df73fd473758fb2401254ad449c9d
Cara Wang <wlin@redhat.com>
2019-05-22 13:12:43 +0800

    add more debug info

--- vars/create_apps.groovy
+++ vars/create_apps.groovy
@@ -1,15 +1,14 @@
 def call(String token, String name, String template, String parameters){
     openshift.withCluster('https://paas.psi.redhat.com', token) {
 	    openshift.withProject('errata-qe-test'){
-	    	echo "here--- $name"
-			echo '--- Create app $name from the $template --->'
+			echo "--- Create app $name from the $template --->"
+			echo "--- With parameters: $parameters ...>"
 			def objectsName
 			if(name.toLowerCase().contains('mysql')){
 				objectsName = (String[]) ["is/${name}", "bc/${name}", "dc/${name}", "svc/${name}"]
 			} else{
 				objectsName = (String[]) ["is/${name}-s2i", "is/${name}-basic", "bc/${name}-bc", "dc/${name}-rails", "route/${name}-route", "svc/${name}-svc"]
 			}
-			echo objectsName.toString()
 
 		    def templateGeneratedSelector = openshift.selector(objectsName)
 		    def objectModels = openshift.process(template, parameters)
