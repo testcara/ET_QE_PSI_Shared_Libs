381647bab8fe85ac8cd21e5168ca29b258cf39af
Cara Wang <wlin@redhat.com>
2019-05-27 12:01:11 +0800

    specify the python version to python3

--- vars/run_ts2_testing.groovy
+++ vars/run_ts2_testing.groovy
@@ -44,7 +44,7 @@ def call(String token, String app_name, String etPod, String casesFeatures){
       ./cucumber_report.sh || true
       # after the rerun, let us use the paraser to merge the rerun.json and cucumber-report.json
       cp /tmp/TS2_db/cucumber_rerun_parser.py .
-      python cucumber_rerun_parser.py --rerun-report=rerun.json --origin-report=cucumber-report-${app_name}.json --new-report=${app_name}-new-report.json
+      python3 cucumber_rerun_parser.py --rerun-report=rerun.json --origin-report=cucumber-report-${app_name}.json --new-report=${app_name}-new-report.json
       rm -rf cucumber-report-${app_name}.json rerun.json
       mv ${app_name}-new-report.json cucumber-report-${app_name}.json
       '''
