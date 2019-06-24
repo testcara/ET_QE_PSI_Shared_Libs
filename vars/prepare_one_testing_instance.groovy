8641e21a939e5a9e08570ab692acf0a86d01294a
Cara Wang <wlin@redhat.com>
2019-06-24 14:03:55 +0800

    add shared lib 'prepare_one_testing_instance()'

---
+++ vars/prepare_one_testing_instance.groovy
@@ -0,0 +1,44 @@
+def call(){
+  podTemplate(label: runner,
+    containers: [
+    containerTemplate(
+        name: 'qe-testing-runner',
+        image: 'docker-registry.upshift.redhat.com/errata-qe-test/qe_testing_upshift_runner:latest',
+        alwaysPullImage: true,
+        command: 'cat',
+        ttyEnabled: true,
+
+        envVars: [
+            envVar(key: 'GIT_SSL_NO_VERIFY', value: 'true')
+        ]
+
+        )],
+    volumes: [
+    persistentVolumeClaim(
+        claimName: 'et-qe-testing-mysql',
+        mountPath: '/tmp/TS2_db/'),
+    persistentVolumeClaim(
+        claimName: 'pvc-errata-qe-test-mnt-redhat',
+        mountPath: '/mnt/redhat')
+    ])
+  {
+      node(runner) {
+      try{
+              stage('TS2 testing preparation'){
+                  container('qe-testing-runner'){
+                        sh '''
+                        git clone https://code.engineering.redhat.com/gerrit/errata-rails
+                        cd errata-rails
+                        git checkout develop
+                        RAILS_ENV=test bundle install
+                        sleep 7200
+                        '''
+                  } //container
+              } //stage
+            } //try
+            catch(Exception e) {
+                echo "Failed to do QE testing ..."
+            }
+      } //node
+    } //container
+} //call
