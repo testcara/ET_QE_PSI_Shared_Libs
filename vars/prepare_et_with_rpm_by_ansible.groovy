41b74d7f02f2beb1d10f6694ea4f3a01cd040d62
Cara Wang <wlin@redhat.com>
2019-07-05 20:50:37 +0800

    add the et server and version to the script

--- vars/prepare_et_with_rpm_by_ansible.groovy
+++ vars/prepare_et_with_rpm_by_ansible.groovy
@@ -1,4 +1,6 @@
-def call(){
+def call(String et_server, String et_version=""){
+  sh "echo export ET_Testing_Server=$et_server > /home/jenins/.bashrc"
+  sh "echo export et_build_name_or_id=$et_version >> /home/jenkins/.bashrc"
   def runner = "mypod-${UUID.randomUUID().toString()}"
   podTemplate(label: runner,
   containers: [
@@ -17,12 +19,15 @@ def call(){
   {
     node(runner) {
       script{
-      sh '''
+        sh '''
+        source /home/jenkins/.bashrc
         git config --global http.sslVerify false
         git clone https://gitlab.infra.prod.eng.rdu2.redhat.com/ansible-playbooks/errata-tool-playbooks.git
         wget http://github.com/testcara/RC_CI/archive/master.zip
-        sleep 7200
-      '''
+        unzip master.zip
+        export WORKSPACE=`pwd`
+        RC_CI-master/auto_testing_CI/prepare_et_server_with_rpm_by_ansible.sh
+        '''
       }
     } // node
   } //pod
