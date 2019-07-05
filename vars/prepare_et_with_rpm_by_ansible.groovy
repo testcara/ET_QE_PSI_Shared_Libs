1b816adf0dd0aa13d1cc5b48aacecf7bc6afefe5
Cara Wang <wlin@redhat.com>
2019-07-05 19:34:28 +0800

    add the function to prepare et

---
+++ vars/prepare_et_with_rpm_by_ansible.groovy
@@ -0,0 +1,23 @@
+def call(){
+  podTemplate(label: runner,
+  containers: [
+  containerTemplate(
+    name: 'et-ansible-runner',
+    image: 'docker-registry.upshift.redhat.com/errata-qe-test/et_ansible_runner:latest',
+    alwaysPullImage: true,
+    command: 'cat',
+    ttyEnabled: true,
+    envVars: [
+      envVar(key: 'GIT_SSL_NO_VERIFY', value: 'true')
+    ]
+
+    )]
+  )
+  {
+    node(runner) {
+      script{
+        sleep(7200)
+      }
+    } // node
+  } //pod
+} //call
