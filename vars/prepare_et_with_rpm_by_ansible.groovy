69efa21f4cbba039b651f4e5d651fc809924c66a
Cara Wang <wlin@redhat.com>
2019-07-05 21:27:53 +0800

    specify the container

--- vars/prepare_et_with_rpm_by_ansible.groovy
+++ vars/prepare_et_with_rpm_by_ansible.groovy
@@ -11,27 +11,27 @@ def call(String et_server, String et_version){
     envVars: [
       envVar(key: 'GIT_SSL_NO_VERIFY', value: 'true')
     ]
-
     )]
   )
   {
     node(runner) {
-      sh "echo $et_server > et_server"
-      sh "echo $et_version > et_version"
-      sh '''
-        whoami || true
-		sleep 3600
-        /usr/local/bin/ci-3-jenkins-slave
-        whoami || true
-        export ET_Testing_Server=$(cat et_server)
-        export et_build_name_or_id=$(cat et_version)
-        git config --global http.sslVerify false
-        git clone https://gitlab.infra.prod.eng.rdu2.redhat.com/ansible-playbooks/errata-tool-playbooks.git
-        wget http://github.com/testcara/RC_CI/archive/master.zip
-        unzip master.zip
-        export WORKSPACE=`pwd`
-        RC_CI-master/auto_testing_CI/prepare_et_server_with_rpm_by_ansible.sh
-        '''
+      container('et-ansible-runner'){
+        sh "echo $et_server > et_server"
+        sh "echo $et_version > et_version"
+        sh '''
+          whoami || true
+          ci-3-jenkins-slave
+          whoami
+          export ET_Testing_Server=$(cat et_server)
+          export et_build_name_or_id=$(cat et_version)
+          git config --global http.sslVerify false
+          git clone https://gitlab.infra.prod.eng.rdu2.redhat.com/ansible-playbooks/errata-tool-playbooks.git
+          wget http://github.com/testcara/RC_CI/archive/master.zip
+          unzip master.zip
+          export WORKSPACE=`pwd`
+          RC_CI-master/auto_testing_CI/prepare_et_server_with_rpm_by_ansible.sh
+          '''
+        } //container
     } // node
   } //pod
 } //call
