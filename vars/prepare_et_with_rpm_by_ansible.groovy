213a48bf96ea66bbd018f94320b7610cd57de425
Cara Wang <wlin@redhat.com>
2019-07-05 20:54:15 +0800

    fix the error

--- vars/prepare_et_with_rpm_by_ansible.groovy
+++ vars/prepare_et_with_rpm_by_ansible.groovy
@@ -1,6 +1,4 @@
 def call(String et_server, String et_version=""){
-  sh "echo export ET_Testing_Server=$et_server > /home/jenins/.bashrc"
-  sh "echo export et_build_name_or_id=$et_version >> /home/jenkins/.bashrc"
   def runner = "mypod-${UUID.randomUUID().toString()}"
   podTemplate(label: runner,
   containers: [
@@ -20,6 +18,8 @@ def call(String et_server, String et_version=""){
     node(runner) {
       script{
         sh '''
+        sh "echo export ET_Testing_Server=$et_server > /home/jenins/.bashrc"
+        sh "echo export et_build_name_or_id=$et_version >> /home/jenkins/.bashrc"
         source /home/jenkins/.bashrc
         git config --global http.sslVerify false
         git clone https://gitlab.infra.prod.eng.rdu2.redhat.com/ansible-playbooks/errata-tool-playbooks.git
