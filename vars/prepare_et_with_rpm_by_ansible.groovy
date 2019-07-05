5879d9558b6f13b994abd20528d7787bde06ea97
Cara Wang <wlin@redhat.com>
2019-07-05 21:07:02 +0800

    fix the syntax error

--- vars/prepare_et_with_rpm_by_ansible.groovy
+++ vars/prepare_et_with_rpm_by_ansible.groovy
@@ -1,6 +1,4 @@
 def call(String et_server, String et_version){
-  sh "echo $et_server > et_server"
-  sh "echo $et_version > et_version"
   def runner = "mypod-${UUID.randomUUID().toString()}"
   podTemplate(label: runner,
   containers: [
@@ -18,17 +16,18 @@ def call(String et_server, String et_version){
   )
   {
     node(runner) {
-      script{
-        sh '''
+      sh "echo $et_server > et_server"
+      sh "echo $et_version > et_version"
+      sh '''
         export ET_Testing_Server=$(cat et_server)
         export et_build_name_or_id=$(cat et_version)
+        git config --global http.sslVerify false
         git clone https://gitlab.infra.prod.eng.rdu2.redhat.com/ansible-playbooks/errata-tool-playbooks.git
         wget http://github.com/testcara/RC_CI/archive/master.zip
         unzip master.zip
         export WORKSPACE=`pwd`
         RC_CI-master/auto_testing_CI/prepare_et_server_with_rpm_by_ansible.sh
         '''
-      }
-    } // node
+    } //node
   } //pod
 } //call
