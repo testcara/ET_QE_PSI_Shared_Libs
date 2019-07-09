fdc675e604cdc70668976756914f6164ba1353f6
Cara Wang <wlin@redhat.com>
2019-07-09 22:22:26 +0800

    auto to get the latest dev build before to send report

--- vars/collect_results_and_publish_testing_report_to_confluence.groovy
+++ vars/collect_results_and_publish_testing_report_to_confluence.groovy
@@ -1,4 +1,4 @@
-def call(String et_build_name_or_id, String jenkins_username, String jenkins_password, String confluence_username, String confluence_password, String space='PDT', String parent_page='56821226'){
+def call(String psi_jenkins_username, String psi_jenkins_password, String confluence_username, String confluence_password, String dev_jenkins_user, String dev_jenkins_password, String dev_jenkins_job='', String et_build_name_or_id="", String space='PDT', String parent_page='56821226'){
   def runner = "mypod-${UUID.randomUUID().toString()}"
   podTemplate(label: runner,
   containers: [
@@ -18,10 +18,13 @@ def call(String et_build_name_or_id, String jenkins_username, String jenkins_pas
     node(runner) {
       container('et-python2-runner'){
         sh "echo $et_build_name_or_id > et_build_name_or_id"
-        sh "echo $jenkins_username > jenkins_username"
-        sh "echo $jenkins_password > jenkins_password"
+        sh "echo $psi_jenkins_username > psi_jenkins_username"
+        sh "echo $psi_jenkins_password > psi_jenkins_password"
         sh "echo $confluence_username > confluence_username"
         sh "echo $confluence_password > confluence_password"
+        sh "echo $dev_jenkins_username > dev_jenkins_username"
+        sh "echo $dev_jenkins_password > dev_jenkins_password"
+        sh "echo $dev_jenkins_job > dev_jenkins_job" 
         sh "echo $space > space"
         sh "echo $parent_page > parent_page"
         sh '''
@@ -31,10 +34,13 @@ def call(String et_build_name_or_id, String jenkins_username, String jenkins_pas
 
           source /etc/bashrc
           export et_build_name_or_id=$(cat et_build_name_or_id)
-          export jenkins_username=$(cat jenkins_username)
-          export jenkins_password=$(cat jenkins_password)
+          export jenkins_username=$(cat psi_jenkins_username)
+          export jenkins_password=$(cat psi_jenkins_password)
           export confluence_username=$(cat confluence_username)
           export confluence_password=$(cat confluence_password)
+          export dev_jenkins_username=$(cat dev_jenkins_username)
+          export dev_jenkins_password=$(cat dev_jenkins_password)
+          export dev_jenkins_job=$(cat dev_jenkins_job)
           export space=$(cat space)
           export parent_page=$(cat parent_page)
           export RC_Jenkins_URL="https://jenkins-errata-qe-test.cloud.paas.psi.redhat.com"
@@ -45,11 +51,15 @@ def call(String et_build_name_or_id, String jenkins_username, String jenkins_pas
           source RC_CI-master/auto_testing_CI/CI_Shell_prepare_env_and_scripts.sh
           source RC_CI-master/auto_testing_CI/CI_Shell_common_usage.sh
 
-          et_build_version=""
+
           install_scripts_env
           et_build_version=$(initial_et_build_version ${et_build_name_or_id})
-          title="ET Testing Reports For Build ${et_build_version}"
+          if [[ "$et_build_version" == "" ]]
+          then
+            et_build_version=$(python RC_CI-master/auto_testing_CI/talk_to_rc_jenkins_to_get_the_latest_dev_build.py ${dev_jenkins_user} ${dev_jenkins_user_token} ${dev_jenkins_job})
+          fi
 
+          title="ET Testing Reports For Build ${et_build_version}"
           echo "Collecting all results and generate the report "
           python RC_CI-master/auto_testing_CI/collect_all_reports_and_update_to_confluence_psi.py "${jenkins_username}" "${jenkins_password}" "${confluence_username}" "${confluence_password}" "${et_build_version}" "${title}" "${space}" "${parent_page}"
 
