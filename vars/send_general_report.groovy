594fdec587475f9dc0302b917ed692ef1ff5d446
Cara Wang <wlin@redhat.com>
2019-07-15 15:12:42 +0800

    print the body for debug

--- vars/send_general_report.groovy
+++ vars/send_general_report.groovy
@@ -1,5 +1,4 @@
 def call(String mail_to){
-  script {
     def test_log = currentBuild.rawBuild.getLog(20)
     sh "echo $test_log > test_log"
     def build_version = sh returnStdout: true, script: '''
@@ -29,7 +28,7 @@ def call(String mail_to){
 
     echo "Sending mail now ..."
     String subject = "Testing Report for Build-" + build_version + "-" + general_status
-    
+
     body = """
     <p style='font-family:arial'>
     <p>ET Version: $build_version</p>
@@ -38,11 +37,13 @@ def call(String mail_to){
     <p>Testing Report: <a href="$report_link">$title</a></p>
     </p>
     """
-    
+
+    echo "Sending mail with body ..."
+    echo body
+
     if (mail_to != null && !mail_to.isEmpty()) {
       // Email on any failures, and on first success.
       mail to: mail_to, subject: subject, body: body, mimeType: "text/html"
       echo 'Sent email notification'
     }
-  }
 }
