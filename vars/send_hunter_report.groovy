1cddd053835d3d4853a71c191a715552b7ecf6cc
testcara <wlin@redhat.com>
2019-09-06 11:31:40 +0800

    fix the syntax error

--- vars/send_hunter_report.groovy
+++ vars/send_hunter_report.groovy
@@ -7,6 +7,7 @@ def call(String api_username, String api_token, String mail_to, String testing_t
        echo "$workspace"
        sh "echo $branch > current_branch"
        latestCommit = sh returnStdout: true, script: '''
+       current_branch=$(cat current_branch)
        if [[ -d 'errata-rails' ]]
        then
          cd errata-rails
@@ -15,7 +16,6 @@ def call(String api_username, String api_token, String mail_to, String testing_t
          cd errata-rails
        fi
        # Without >> /dev/null, the output of this step makes the latestCommit contain some unexpected strings
-       current_branch=$(cat current_branch)
        git checkout $current_branch >> /dev/null
        git pull || true >> /dev/null
        commit=$(git rev-parse HEAD)
