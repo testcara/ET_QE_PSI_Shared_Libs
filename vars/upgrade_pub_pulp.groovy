32830c402ef785d36be424a66560ae593903cb45
Cara Wang <wlin@redhat.com>
2019-09-24 10:13:37 +0800

    remove pulp and pulp docker related parameters

--- vars/upgrade_pub_pulp.groovy
+++ vars/upgrade_pub_pulp.groovy
@@ -1,4 +1,4 @@
-def call(String pubServer, String pulpServer, String pulpDockerServer, String pubJenkinsBuild="", String pulpBuildForRPM="", String pulpRPMBuild="", String pulpBuildForDocker="", String pulpDockerBuild=""){
+def call(String pubServer, String pulpServer, String pulpDockerServer, String pubJenkinsBuild="") {
 
     def runner_1 = "mypod-${UUID.randomUUID().toString()}"
     podTemplate(label: runner_1,
@@ -21,11 +21,7 @@ def call(String pubServer, String pulpServer, String pulpDockerServer, String pu
                      string(name: 'pub_server', defaultValue: pubServer),
                      string(name: 'pulp_rpm_server', defaultValue: pulpServer),
                      string(name: 'pulp_docker_server', defaultValue: pulpDockerServer),
-                     string(name: 'pub_jenkins_build', defaultValue: pubJenkinsBuild),
-                     string(name: 'pulp_build_for_rpm', defaultValue: pulpBuildForRPM),
-                     string(name: 'pulp_rpm_build', defaultValue: pulpRPMBuild),
-                     string(name: 'pulp_build_for_docker', defaultValue: pulpBuildForDocker),
-                     string(name: 'pulp_docker_build', defaultValue: pulpDockerBuild)
+                     string(name: 'pub_jenkins_build', defaultValue: pubJenkinsBuild)
                      ]
                     )
             ])
@@ -39,10 +35,6 @@ def call(String pubServer, String pulpServer, String pulpDockerServer, String pu
             sh "echo $pulp_rpm_server > pulp_rpm_server"
             sh "echo $pulp_docker_server > pulp_docker_server"
             sh "echo $pub_jenkins_build > pub_jenkins_build"
-            sh "echo $pulp_build_for_rpm > pulp_build_for_rpm"
-            sh "echo $pulp_rpm_build > pulp_rpm_build"
-            sh "echo $pulp_build_for_docker > pulp_build_for_docker"
-            sh "echo $pulp_docker_build > pulp_docker_build"
 
             sh '''
             user_id=$(id | cut -d " " -f 1 | cut -d "=" -f 2 | tr -d ' ')
@@ -55,14 +47,9 @@ def call(String pubServer, String pulpServer, String pulpDockerServer, String pu
             export pulp_rpm_server=$(cat ${CI3_WORKSPACE}/pulp_rpm_server)
             export pulp_docker_server=$(cat ${CI3_WORKSPACE}/pulp_docker_server)
             export pub_jenkins_build=$(cat ${CI3_WORKSPACE}/pub_jenkins_build)
-            export pulp_build_for_rpm=$(cat ${CI3_WORKSPACE}/pulp_build_for_rpm)
-            export pulp_rpm_build=$(cat ${CI3_WORKSPACE}/pulp_rpm_build)
-            export pulp_build_for_docker=$(cat ${CI3_WORKSPACE}/pulp_build_for_docker)
-            export pulp_docker_build=$(cat ${CI3_WORKSPACE}/pulp_docker_build)
 
             cd RC_CI-master/auto_testing_CI/
             ./upgrade_pub_pulp_psi.sh
-            #./prepare_pulp_pulp_db_and_services.sh
             '''
             }
         }
