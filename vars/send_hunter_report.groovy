8af5c548f9350a0d661b98a73af52d23ae116506
Cara Wang <wlin@redhat.com>
2019-09-24 14:03:57 +0800

    force checkout the branch

--- vars/send_hunter_report.groovy
+++ vars/send_hunter_report.groovy
@@ -16,7 +16,7 @@ def call(String api_username, String api_token, String mail_to, String testing_t
          cd errata-rails
        fi
        # Without >> /dev/null, the output of this step makes the latestCommit contain some unexpected strings
-       git checkout $current_branch >> /dev/null
+       git checkout $current_branch -f >> /dev/null
        git pull || true >> /dev/null
        commit=$(git rev-parse HEAD)
        echo $commit
