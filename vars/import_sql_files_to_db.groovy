2798961dc9abc22e92786560823ed051289af1f3
testcara <wlin@redhat.com>
2019-10-09 10:01:59 +0800

    support the mariadb

--- vars/import_sql_files_to_db.groovy
+++ vars/import_sql_files_to_db.groovy
@@ -3,7 +3,7 @@ def call(String token, String podName, String sqlFile, String mysqlUser, String 
     openshift.withProject('errata-qe-test'){
       //sh "oc exec ${podName} -i -- mysql -u${mysqlUser} -p${mysqlPassword} -Derrata < ${sqlFile}"
       // for mariadb, we need to specify the absolute path of mysql and import the data without password
-      sh "oc exec ${podName} -i -- /opt/rh/rh-mariadb102/root/usr/bin/mysql -u${mysqlUser}  -Derrata < ${sqlFile}" 
+      sh "oc exec ${podName} -i -- /opt/rh/rh-mariadb102/root/usr/bin/mysql -uroot -Derrata < ${sqlFile}" 
     }
   }
 }
