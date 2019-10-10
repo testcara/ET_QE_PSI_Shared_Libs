e06aaa3954198b6e7007795f76482c3a90bad99f
testcara <wlin@redhat.com>
2019-10-10 10:26:06 +0800

    fix the format issues

--- vars/upload_templates.groovy
+++ vars/upload_templates.groovy
@@ -1,6 +1,5 @@
 def call(String token, String template){
-//def call(String token, String template, String templatePathofMysql){
-	openshift.withCluster('https://paas.psi.redhat.com', token) {
+  openshift.withCluster('https://paas.psi.redhat.com', token) {
         openshift.withProject('errata-qe-test'){
 	        echo '--- Upload template --->'
 	        openshift.create(template)
