beb83102da7b6eb368042ff36a8679ac354837b3
Cara Wang <wlin@redhat.com>
2019-10-10 16:42:09 +0800

    add 'track_deployment' dc

---
+++ vars/track_deployment.groovy
@@ -0,0 +1,28 @@
+def call(String token, String dcName){
+    openshift.withCluster('https://paas.psi.redhat.com', token) {
+        openshift.withProject('errata-qe-test'){
+        sh "echo $dcName > dcName"
+        sh '''
+            dcName=$(cat dcName)
+            time=$(cat time)
+            # let us wait 5 mins
+            for i in {1..5}
+            do
+                sleep 60 # 10 seconds
+                status=$(oc get pods | grep ${dcName} | grep -v build | grep -v deploy | awk "{print $3}")
+                if [[ ${status} =~ "Running" ]]
+                then
+                  echo "---> Deployment complete ..."
+                  exit 0
+                elif [[ ${status} =~ "Failed" ]] || [[ ${status} =~ "Error" ]]
+                then
+                  echo "---> Deployment has been failed ..."
+                  exit 1
+                else
+                  echo "---> Still running ..."
+                fi
+            done
+           '''
+        }//project
+    }//cluster
+}//call
