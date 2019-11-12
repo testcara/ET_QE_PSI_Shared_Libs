7ab86783096a6455b644c3eb7c3da54300215068
Cara Wang <wlin@redhat.com>
2019-11-12 16:51:12 +0800

    add the branch parameter

--- vars/run_ts2_testing_against_existing_pod.groovy
+++ vars/run_ts2_testing_against_existing_pod.groovy
@@ -1,12 +1,11 @@
-def call(String token, String appName, String etPod, String casesFeatures){
+def call(String token, String appName, String etPod, String branch, String casesFeatures){
 
-	echo "---> Now, you are using the ET pipeline shared lib ..."
-
-	def RUN_USER = '1058980001'
-	def MSYQL_USER = "root"
-	def MYSQL_PASSWORD = "arNdk123_"
-	def runner= "mypod-${UUID.randomUUID().toString()}"
-	podTemplate(label: runner,
+  echo "---> Now, you are using the ET pipeline shared lib ..."
+  def RUN_USER = '1058980001'
+  def MSYQL_USER = "root"
+  def MYSQL_PASSWORD = "arNdk123_"
+  def runner= "mypod-${UUID.randomUUID().toString()}"
+  podTemplate(label: runner,
     containers: [
     containerTemplate(
         name: 'qe-testing-runner',
@@ -25,38 +24,38 @@ def call(String token, String appName, String etPod, String casesFeatures){
         claimName: 'pvc-errata-qe-test-mnt-redhat',
         mountPath: '/mnt/redhat')
     ])
-	{
-	    node(runner) {
-	    	try{
-	            stage('TS2 testing preparation'){
-	                container('qe-testing-runner'){
-
-                        echo "Add the pulp configuration files to runner"
-                        sh """
-                        mkdir ~/.rcm
-                        cp /tmp/pulp_configs/.rcm/pulp-environments.json ~/.rcm/
-                        """
-
-	                }
-	            }
-                stage('run TS2 testing'){
-                    container('qe-testing-runner'){
-                        sh '''
-                        git clone https://code.engineering.redhat.com/gerrit/errata-rails
-                        cd errata-rails
-                        git checkout develop
-                        '''
-                        run_ts2_testing(token, appName, etPod, casesFeatures)
-                    }
-                } //stage
-            } //try
-            catch(Exception e) {
-                echo "Failed to do QE testing ..."
-            }
-            finally{
-                archiveArtifacts '**/cucumber-report*.json'
-                cucumber fileIncludePattern: "**/cucumber-report*.json", sortingMethod: "ALPHABETICAL"
-            }
-	    } //node
-	  } //container
+  {
+      node(runner) {
+        try{
+              stage('TS2 testing preparation'){
+                container('qe-testing-runner'){
+                  echo "Add the pulp configuration files to runner"
+                  sh """
+                  mkdir ~/.rcm
+                  cp /tmp/pulp_configs/.rcm/pulp-environments.json ~/.rcm/
+                  """
+                }
+              }
+              stage('run TS2 testing'){
+                container('qe-testing-runner'){
+                  sh "echo $branch >> branch"
+                  sh '''
+                  branch=$(cat branch)
+                  git clone https://code.engineering.redhat.com/gerrit/errata-rails
+                  cd errata-rails
+                  git checkout ${branch}
+                  '''
+                  run_ts2_testing(token, appName, etPod, casesFeatures)
+                }
+             } //stage
+         } //try
+        catch(Exception e) {
+              echo "Failed to do QE testing ..."
+        }
+        finally{
+              archiveArtifacts '**/cucumber-report*.json'
+              cucumber fileIncludePattern: "**/cucumber-report*.json", sortingMethod: "ALPHABETICAL"
+        }
+      } //node
+    } //container
 } //call
