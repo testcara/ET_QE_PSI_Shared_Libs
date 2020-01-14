0937c19246e19552f64a77c01b5738a5f721c4c8
Cara Wang <wlin@redhat.com>
2020-01-14 10:38:06 +0800

    fix the syntax error

--- vars/ET_Pipeline.groovy
+++ vars/ET_Pipeline.groovy
@@ -1,7 +1,7 @@
 def call(String token, String bc_strategy, String appName, String templateNameofET, String templateNameofMysql,
     String etTemplateParameters, String mysqlTemplateParameters,
 		String templatePathofET, String templatePathofMysql,
-		String qeTesting, String casesTags, String parallel, String current_branch, String remove_pods){++
+		String qeTesting, String casesTags, String parallel, String current_branch, String remove_pods){
 
 	  echo "---> Now, you are using the ET pipeline shared lib ..."
 
