ad4219667eae2f2fb0528cc7f8e5a67bd9dc2158
carawang <carawang@redhat.com>
2020-02-10 16:00:38 +0800

    ignore remove pods

--- vars/ET_Pipeline.groovy
+++ vars/ET_Pipeline.groovy
@@ -1,6 +1,6 @@
 def call(String token, String project_name, String bc_strategy, String appName, String templateNameofET,
     String etTemplateParameters, String templatePathofET, String qeTesting, String casesTags,
-    String parallel, String current_branch, String remove_pods){
+    String parallel, String current_branch){
 
 	  echo "---> Now, you are using the ET pipeline shared lib ..."
 
@@ -278,19 +278,6 @@ mountPath: '/mnt/brew'),
         archiveArtifacts '**/cucumber-report*.json'
         cucumber fileIncludePattern: "**/cucumber-report*.json", sortingMethod: "ALPHABETICAL"
         clean_ws()
-        if(remove_pods=="true"){
-          container('qe-testing-runner'){
-            retry(2) {
-              [appName, "${appName}-mariadb-102-rhel7"].each {
-                if(parallel=="true"){
-                  clean_up_by_oc(project_name, it, 'app')
-                } else{
-                  clean_up(project_name, it, 'app')
-                } //if
-              } //each
-            } //retry
-          } //container
-        }//if_remove_pods
       } // finally
     } //node
   } //containerTemplate
